a8e2990029dfd5f9f4d27cf8b6f9ba99fcf64393
zahid-soft <zahidsarang95@gmail.com>
2023-10-04 18:24:19 +0530

    test lint stage

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ function login(username: string): string {
     const user = {
         name: "zhid",
     };
-    const name = user["name"];
+    const name = user.name;
     return username + name;
 }
 
