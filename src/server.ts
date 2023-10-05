b627c2f973db3a5ccc4c510c409b213c71613c2b
zahid-soft <zahidsarang95@gmail.com>
2023-10-05 10:15:10 +0530

    add winston

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import app from "./app";
 import { Config } from "./config";
+import logger from "./config/logger";
 
 const startServer = () => {
     const PORT = Config.PORT;
     try {
-        // eslint-disable-next-line no-console
-        app.listen(PORT, () => console.log("Starting server", PORT));
-    } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error("Error", error);
-        process.exit(1);
+        throw new Error("something went wrong");
+        logger.debug("debug message", {});
+        app.listen(PORT, () => logger.info(`Listing on ${PORT}`));
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            logger.error("Error", error);
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000);
+        }
     }
 };
 
