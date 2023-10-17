9cf58609ae506e4598527bbdb9067f7f9ed767da
zahid-soft <zahidsarang95@gmail.com>
2023-10-17 21:29:52 +0530

    db connection to server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import app from "./app";
 import { Config } from "./config";
+import { AppDataSource } from "./config/data-source";
 import logger from "./config/logger";
 
-const startServer = () => {
+const startServer = async () => {
     const PORT = Config.PORT;
     try {
+        await AppDataSource.initialize();
+        logger.info("database connected successfully!");
         app.listen(PORT, () => logger.info(`Listing on ${PORT}`));
     } catch (error: unknown) {
         if (error instanceof Error) {
@@ -16,4 +19,4 @@ const startServer = () => {
     }
 };
 
-startServer();
+void startServer();
