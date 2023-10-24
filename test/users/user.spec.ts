8c1d48fd7521813eeeb787aa3716e10572b85ce6
zahid-soft <zahidsarang95@gmail.com>
2023-10-24 16:06:06 +0530

    write a test for return the user data

--- test/users/user.spec.ts
+++ test/users/user.spec.ts
@@ -1,20 +1,28 @@
+import request from "supertest";
+import createJWKSMock from "mock-jwks";
 import app from "../../src/app";
 import { AppDataSource } from "../../src/config/data-source";
 import { DataSource } from "typeorm";
-import request from "supertest";
-// import { User } from "../../src/entity/User";
-// import { RefreshToken } from "../../src/entity/RefreshToken";
+import { User } from "../../src/entity/User";
+import { Roles } from "../../src/constants";
 
 describe("Get /auth/self", () => {
     let connection: DataSource;
+    let jwks: ReturnType<typeof createJWKSMock>;
 
     beforeAll(async () => {
+        jwks = createJWKSMock("http://localhost:8001");
         connection = await AppDataSource.initialize();
     });
 
     beforeEach(async () => {
+        jwks.start();
         await connection.dropDatabase();
         await connection.synchronize();
+    });
+
+    afterEach(() => {
+        jwks.stop();
     });
 
     afterAll(async () => {
@@ -25,6 +33,28 @@ describe("Get /auth/self", () => {
         it("should return the 200 status code", async () => {
             const response = await request(app).get("/auth/self").send();
             expect(response.statusCode).toBe(200);
+        });
+
+        it("should return the user data", async () => {
+            // register user
+            const userData = {
+                firstName: "zahid",
+                lastName: "sarang",
+                email: "zahid@gmail.com",
+                password: "password",
+            };
+            const userRepository = connection.getRepository(User);
+            const data = await userRepository.save({ ...userData, role: Roles.CUSTOMER });
+            // generate token
+            const accessToken = jwks.token({ sub: String(data.id), role: data.role });
+            // add token to cookie
+            const response = await request(app)
+                .get("/auth/self")
+                .set("Cookie", [`accessToken=${accessToken}`])
+                .send();
+            // assert
+            // check if user id matches with register user
+            expect((response.body as Record<string, string>).id).toBe(data.id);
         });
     });
 });
